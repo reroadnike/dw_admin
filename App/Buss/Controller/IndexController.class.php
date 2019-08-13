78bf7a4418a5fe588169535d734ffac89b5af449
reroadnike <1531900560@qq.com>
2019-08-13 09:50:56 +0800

    测试分支

--- App/Buss/Controller/IndexController.class.php
+++ App/Buss/Controller/IndexController.class.php
@@ -3,10 +3,10 @@ namespace Buss\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        echo "新增了一个模块success!!!!!!!";
+        
     }
 
     public function test() {
-    	echo '测试跑通';
+    	
     }
 }
