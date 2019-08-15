8f42a86cf3dd10d7dd6948ff01a8d1be14fb43c1
reroadnike <1531900560@qq.com>
2019-08-15 16:02:20 +0800

    新增商家管理模块，初期部署

---
+++ App/Home/Controller/BussController.class.php
@@ -0,0 +1,14 @@
+<?php
+/*
+**Author:lzp
+**Time:2019-8-15
+ */
+
+namespace Home\Controller;
+use Home\Controller\BaseController;
+
+class BussController extends BaseController {
+	public function index() {
+		
+	}
+}
