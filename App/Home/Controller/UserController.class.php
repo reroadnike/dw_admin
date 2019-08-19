f7bdc5d9c1359b47ce16efd4ed83aaa035b197a2
reroadnike <1531900560@qq.com>
2019-08-19 14:09:58 +0800

    新增用户控制器

---
+++ App/Home/Controller/UserController.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ **Created by PhpStorm
+ **Author:lzp
+ **Date:2019/8/19 0019
+ **Time:下午 2:04
+ **/
+namespace Home\Controller;
+use Home\Controller\BaseController;
+
+class UserController extends BaseController {
+
+}
